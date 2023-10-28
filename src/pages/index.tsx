3b54ae61542af90d5124ba94d0852b468dd0610a
Brandon Owens <brandono@vitamins2you.com>
2023-10-27 20:24:59 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,20 +54,20 @@ const Header = () => {
             </div>
           </main>
           <div className="border-t border-zinc-100 pb-8 pt-8 dark:border-zinc-700/40">
-          <div className="flex flex-row items-center justify-center text-sm font-regular text-zinc-800 dark:text-zinc-200">
-            Powered by 
-            <a 
-              target="_blank" 
-              rel="noreferrer"
-              href="http://elegantframework.com/" 
-              aria-label="Powered by the Elegant framework"
-              className="font-semibold hover:text-white dark:hover:text-white pl-1"
-            >
-                Elegant
-            </a>
-              .
+            <div className="flex flex-row items-center justify-center text-sm font-regular text-zinc-200 dark:text-zinc-200">
+              Powered by 
+              <a 
+                target="_blank" 
+                rel="noreferrer"
+                href="http://elegantframework.com/" 
+                aria-label="Powered by the Elegant framework"
+                className="font-semibold hover:text-white dark:hover:text-white pl-1"
+              >
+                  Elegant
+              </a>
+                .
+            </div>
           </div>
-        </div>
         </div>
       </header>
     </React.Fragment>
